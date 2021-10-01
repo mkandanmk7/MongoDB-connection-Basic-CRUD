10a98b854ca937e1ddece9cda7c53c7d597f5107
muthu <mkandanmk7@gmail.com>
2021-10-01 16:44:00 +0530

    service done

--- Router/Users.js
+++ Router/Users.js
@@ -1,60 +1,16 @@
-const userApi = require("express").Router();
+const userApi = require("express").Router(); //for Routes
 
-const { ObjectId } = require("mongodb"); //driver
-
-const db = require("../mongo"); // mongo db connection
+const service = require("../Service/user.service");
 
 //post Api Routes;
 // get methods
 
-userApi.get("/", async (req, res) => {
-  try {
-    // find  make it as array : toArray()
-    const getData = await db.users.find().toArray(); // use toArray( ) spl for find( ) query
+userApi.get("/", service.getData);
 
-    // console.log(getData);
-    res.send(getData); // make it as array;   data to fEnd;
-  } catch (err) {
-    res.sendStatus(500); // send error status;
-  }
-});
+userApi.delete("/:id", service.deleteData);
 
-userApi.delete("/:id", async (req, res, next) => {
-  try {
-    console.log("Delete id is :", req.params.id);
-    // deleteOne
-    await db.users.deleteOne({ _id: ObjectId(req.params.id) });
-    res.send({});
-  } catch (err) {
-    res.sendStatus(500);
-  }
-});
+userApi.post("/", service.postData);
 
-userApi.post("/", async (req, res) => {
-  try {
-    const { insertedId: _id } = await db.users.insertOne(req.body);
-
-    //insert
-    res.send({ ...req.body, _id });
-  } catch (err) {
-    res.sendStatus(500);
-  }
-});
-
-userApi.put("/:id", async (req, res) => {
-  try {
-    // console.log(req.params.id); // its string but _id: is Object(id)
-    await db.users.findOneAndUpdate(
-      { _id: ObjectId(req.params.id) },
-      { $set: { ...req.body } },
-      { ReturnDocument: "after" }
-    );
-    // console.log(data);
-    res.send({ ...req.body, id: req.params.id });
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
+userApi.put("/:id", service.updateData);
 
 module.exports = userApi;
